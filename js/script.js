b59f227908f35cf8ae1abf6351abd8d81039d281
BairaTs <tsybenova.baira@mail.ru>
2022-11-17 15:08:42 +0300

    homework 7f

--- js/script.js
+++ js/script.js
@@ -4,5 +4,5 @@ const myHome = "живу в Ясногорске";
 const profession = "инженер по охране труда";
 let now = new Date();
 const birthday = new Date(1995, 7, 26); 
-let strStore = `Меня зовут ${myName}? по профессии я ${profession}, в данный момент. Мне ${now.getFullYear - birthday.getFullYear} лет. ${vopros}`
+let strStore = `Меня зовут ${myName}? по профессии я ${profession}, в данный момент. Мне ${now.getFullYear() - birthday.getFullYear()} лет. ${vopros}`
 console.log(strStore)
